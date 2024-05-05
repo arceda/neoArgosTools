565a36152e16a1b3bfcde487d79878eb4bd7e0c0
arceda <enriquefirst@gmail.com>
2024-05-05 11:26:09 -0500

    backend

--- BACKEND/main.py
+++ BACKEND/main.py
@@ -6,13 +6,14 @@ import os
 fastqc_path = "./FastQC/fastqc"
 new_permissions = 0o755
 
-os.chdir("FastQC")
-os.chmod("fastqc", 0o755)
+#os.chdir("FastQC")
+#os.chmod("fastqc", 0o755)
 
 
 app = Flask(__name__)
 CORS(app)
 
+#path_fastq_files = "../FASTQs/"
 
 @app.route("/fastqc", methods=["POST"])
 @cross_origin()
@@ -22,9 +23,9 @@ def fastqc():
     try:
         data = request.get_data()
         jsonData = json.loads(data)  # Data en formato json para usar con fastqc <3
-        os.system("fastqc FASTQs/*fastq.gz")
-        # os.system("mv FASTQs/*.html RESULTS/FASTQC")
-        # os.system("mv FASTQs/*.zip RESULTS/FASTQC")
+        os.system("fastqc ../FASTQs/*fastq.gz")
+        os.system("mv ../FASTQs/*.html ../RESULTS/FASTQC")
+        os.system("mv ../FASTQs/*.zip ../RESULTS/FASTQC")
 
         d["status"] = 1
 
