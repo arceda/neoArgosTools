fc090ebd2c500bc0e5327a0e1f9d8ebff0524690
Jean Paul <jmamanim@ulasalle.edu.pe>
2024-11-05 17:52:25 -0500

    Desplegando en el 0.0.0.0

--- FLOWGUI/vite.config.js
+++ FLOWGUI/vite.config.js
@@ -1,10 +1,14 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-    plugins: [react()],
-    define: {
-        'process.env': {}
-    },
-    base: './',
+  plugins: [react()],
+  define: {
+    "process.env": {},
+  },
+  base: "./",
+  server: {
+    host: "0.0.0.0",
+    port: 5173,
+  },
 });
